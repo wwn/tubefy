b2b067e68e2e2771337acf37d435bbd804dd3a22
wwn <wwn@mailbox.org>
2026-01-20 19:51:38 +0100

    fix test?

--- src/test/java/ch/nickl/tubefy/infrastructure/config/SubscriptionConfigTest.java
+++ src/test/java/ch/nickl/tubefy/infrastructure/config/SubscriptionConfigTest.java
@@ -2,14 +2,28 @@ package ch.nickl.tubefy.infrastructure.config;
 
 import ch.nickl.tubefy.domain.model.SubscriptionConfig;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
+@TestProfile(SubscriptionConfigTest.Profile.class)
 class SubscriptionConfigTest {
+
+    public static class Profile implements QuarkusTestProfile {
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            return Map.of(
+                    "subscription.config", "[{\"discord_webhook_url\":\"http://test\",\"yt_subscriptions\":[{\"yt_channel_id\":\"UC123\",\"greetingText\":\"test\"}]}]"
+            );
+        }
+    }
 
     @Inject
     @ConfigProperty(name = "subscription.config")
